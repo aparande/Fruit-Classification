5411a1e10db41adcc51813b9e3e4ee881f5b025c
Anmol Parande <parandea1.7@gmail.com>
2018-09-20 00:06:33 -0700

    Recovered classifier.py and dataset.py from server

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import dataset
 import random
+
 import sys
 from tensorflow.python import debug as tf_debug
 
@@ -27,29 +28,34 @@ def conv_layer(input, filter_size, name, strides = [1, 1, 1, 1], pool_size=2, pa
         biases = get_biases([filter_size[3]])
 
         #Convolve the image
-        conv = tf.nn.sigmoid(tf.nn.conv2d(input, weights, strides, padding))
+        #conv = tf.nn.sigmoid(tf.nn.conv2d(input, weights, strides, padding))
+        conv = tf.nn.leaky_relu(tf.nn.conv2d(input, weights, strides, padding))
         #Pools the convoluted layer. Arguments are input, pool_size, strides, padding
         pool = tf.nn.max_pool(conv, [1, pool_size, pool_size, 1], [1, pool_size, pool_size, 1], padding)
 
         return pool
 
-def dense_layer(input, input_size, output_size, name):
+def dense_layer(inputs, input_size, output_size, name):
     with tf.name_scope(name):
         weights = get_weights([input_size, output_size])
         biases = get_biases([output_size])
 
-        h = tf.matmul(input, weights) + biases
-        return tf.nn.sigmoid(h)
+        h = tf.matmul(inputs, weights) + biases
+
+
+        #return tf.nn.sigmoid(h)
+        return tf.nn.leaky_relu(h)
 
 def define_model(images):
-    #Output shape = (batch_size, 50, 50, 10)
-    conv1 = conv_layer(images, [20, 20, 3, 10], "Conv1")
-    #Output shape = (batch_size, 25, 25, 20)
+    #Output shape = (batch_size, 25, 25, 10)
+    conv1 = conv_layer(images, [20, 20, 3, 10], "Conv1", pool_size=4)
+    #Output shape = (batch_size, 13, 13, 20)
     conv2 = conv_layer(conv1, [5, 5, 10, 20], "Conv2")
-    conv2 = tf.reshape(conv2, [-1, 25 * 25 * 20])
+    conv2 = tf.reshape(conv2, [-1, 13 * 13 * 20])
 
-    dense1 = dense_layer(conv2, 25 * 25 * 20, 100, "Dense1")
-    logits = dense_layer(dense1, 100, NUM_CLASSES, "Softmax_Linear")
+    dense1 = dense_layer(conv2, 13 * 13 * 20, 1000, "Dense1")
+    dense2 = dense_layer(dense1, 1000, 100, "Dense2")
+    logits = dense_layer(dense2, 100, NUM_CLASSES, "Softmax_Linear")
 
     return logits
 
@@ -60,10 +66,11 @@ def define_loss(logits, labels):
     loss = tf.reduce_mean(cross_entropy, name="xentropy_mean")
     return loss
 
-def training(loss, learning_rate):
+def training(loss, learning_rate, momentum = 0.75, beta1=0.9, beta2=0.999):
     tf.summary.scalar('loss', loss)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-
+    #optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    #optimizer = tf.train.MomentumOptimizer(learning_rate, momentum)
+    optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1, beta2=beta2)
     train_op = optimizer.minimize(loss)
     return train_op
 
@@ -85,10 +92,11 @@ def do_evaluation(session, eval_correct, testDataset, batch_size, images_placeho
     precision = float(totalCorrect) / len(training_data)
     print('Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' % (len(training_data), totalCorrect, precision))
     sys.stdout.flush()
+    return precision > 0.95
 
 def run_training(batch_size, learning_rate, epochs, run_number):
     with tf.Graph().as_default():
-        images_placeholder, labels_placeholder = placeholder_inputs(batch_size)
+        images_placeholder, labels_placeholder= placeholder_inputs(batch_size)
 
         logits = define_model(images_placeholder)
         lossFunction = define_loss(logits, labels_placeholder)
@@ -96,36 +104,58 @@ def run_training(batch_size, learning_rate, epochs, run_number):
 
         eval_correct = evaluation(logits, labels_placeholder)
 
-        summary = tf.summary.merge_all()
+ #       summary = tf.summary.merge_all()
+        saver = tf.train.Saver()
         init = tf.global_variables_initializer()
 
         with tf.Session() as session:
             #session = tf_debug.LocalCLIDebugWrapperSession(session)
-
-            summary_writer = tf.summary.FileWriter("log/"+str(run_number), session.graph)
+            
+            logdir = "log/"+str(run_number)
+ #           summary_writer = tf.summary.FileWriter(logdir, session.graph)
             session.run(init)
 
             for step in range(epochs):
                 training_data, training_labels = dataset.get_training_data()
                 batches = [(training_data[i:i+batch_size], training_labels[i:i+batch_size]) for i in range(0, len(training_data), batch_size)]
+                epochLoss = 0
                 for batch in batches:
                     image_data = batch[0]
                     label_data = batch[1]
 
                     feed_dict = {images_placeholder: image_data, labels_placeholder: label_data}
                     activations, loss_value = session.run([train_op, lossFunction], feed_dict=feed_dict)
+                    epochLoss += loss_value
                 if step % 2 == 0:
+                    #print('Step %d: loss = %.2f' % (step, epochLoss))
                     print('Step %d: loss = %.2f' % (step, loss_value))
                     sys.stdout.flush()
-                    summary_str = session.run(summary, feed_dict=feed_dict)
-                    summary_writer.add_summary(summary_str, step)
-                    summary_writer.flush()
+#                    summary_str = session.run(summary, feed_dict=feed_dict)
+#                    summary_writer.add_summary(summary_str, step)
+#                    summary_writer.flush()
 
 
+                early_stop = False
                 if (step + 1) % 5 == 0 or (step + 1) == epochs:
                     validation_data = dataset.get_validation_data(batch_size)
                     print("Doing evaluation on validation Set")
                     sys.stdout.flush()
-                    do_evaluation(session, eval_correct, validation_data, batch_size, images_placeholder, labels_placeholder)
+                    early_stop = do_evaluation(session, eval_correct, validation_data, batch_size, images_placeholder, labels_placeholder)
+		
+                if (step + 1) == epochs or early_stop:
+                    print("Doing evaluation on training set")
+                    sys.stdout.flush()
+                    do_evaluation(session, eval_correct, (training_data, training_labels), batch_size, images_placeholder, labels_placeholder)
 
-run_training(59, 0.05, 20, 0)
+                    print("Doing evaluation on the test set")
+                    sys.stdout.flush()
+                    test_data = dataset.get_test_data(batch_size)
+                    do_evaluation(session, eval_correct, test_data, batch_size, images_placeholder, labels_placeholder)
+                    
+                    saver.save(session, "model.ckpt")
+
+                    if (early_stop):
+                        print("Achieved desired precision at step %d" % step)
+                        return
+
+run_training(89, 0.001, 40, 13)
--- dataset.py
+++ dataset.py
@@ -3,10 +3,11 @@ import os
 import numpy as np
 import random
 import pickle
+import h5py as h5
 
 TRAINING_DIR = "fruits-360/Training/"
 TEST_DIR = "fruits-360/Test/"
-CLASSES = ["Banana", "Lemon", "Mango", "Orange", "Pineapple", "Guava", "Carambula", "Cocos", "Granadilla", "Kiwi"]
+CLASSES = ["Banana", "Lemon", "Mango", "Orange", "Pineapple", "Guava", "Carambula", "Cocos", "Granadilla", "Kiwi", "Apple Braeburn", "Apple Granny Smith", "Apple Red 1", "Apricot", "Avocado", "Cactus Fruit", "Cherry 1", "Cherry 2", "Dates", "Cantaloupe 1", "Cantaloupe 2", "Limes", "Lychee", "Plum", "Raspberry", "Tamarillo", "Tangelo", "Pear", "Kumquats", "Mandarine", "Pepino", "Physalis", "Pineapple Mini", "Salak", "Papaya", "Quince", "Kaki", "Maracuja", "Mulberry", "Pomegranate"]
 
 def image_to_array(imagePath):
     #Convert an image path to an array that can be used for classification
@@ -21,47 +22,41 @@ def get_label(label, one_hot=False):
 
 def get_training_data():
     try:
-        training_pickle = open(os.path.join(TRAINING_DIR, "training.pkl"), 'rb')
-        images, labels = pickle.load(training_pickle)
-        #print("Loaded training_data from pickle")
-        training_pickle.close()
-
-        combo = list(zip(images, labels))
-        random.shuffle(combo)
-        images, labels = zip(*combo)
-
-        return images, labels
-    except FileNotFoundError:
-        #print("Constructing training data from scratch")
-        raw_data = []
-        #load the images into a single array
+        training_file = h5.File(TRAINING_DIR+'fruit_data.hdf5', 'r')
+        return training_file["Training"]["images"], training_file["Training"]["labels"]
+    except:
+        print("Constructing training data from scratch")
+        paths_and_labels = [] 
         for subdir, dirs, files in os.walk(TRAINING_DIR):
             for file in files:
                 label = os.path.basename(subdir)
                 if label in CLASSES:
                     path = os.path.join(subdir, file)
-                    raw_data.append((image_to_array(path), get_label(label)))
+                    paths_and_labels.append((path, get_label(label)))
 
-        #Break it into a training set and validation set
-        random.shuffle(raw_data)
-        split_index = len(raw_data) // 5
-        training_data = raw_data[split_index:]
-        validation_data = raw_data[:split_index]
+        random.shuffle(paths_and_labels)
+        split_index = len(paths_and_labels) // 5
 
-        training_images, training_labels = format_data(training_data)
-        validation_images, validation_labels = format_data(validation_data)
+        training_half = paths_and_labels[split_index:]
+        validation_half = paths_and_labels[:split_index]
 
-        #Save each data set
-        training_pickle = open(os.path.join(TRAINING_DIR, "training.pkl"), 'wb')
-        validation_pickle = open(os.path.join(TRAINING_DIR, "validation.pkl"), 'wb')
-
-        pickle.dump((training_images, training_labels), training_pickle)
-        pickle.dump((validation_images, validation_labels), validation_pickle)
-
-        training_pickle.close()
-        validation_pickle.close()
-
+        training_file = h5.File(TRAINING_DIR+'fruit_data.hdf5', 'w')
+        
+        training_images, training_labels = format_data(training_half, training_file, "Training")
+        validation_images, validation_labels = format_data(validation_half, training_file, "Validation")
+        
         return training_images, training_labels
+    #try:
+     #   training_pickle = open(os.path.join(TRAINING_DIR, "training.pkl"), 'rb')
+      #  images, labels = pickle.load(training_pickle)
+       # print("Loaded training_data from pickle")
+        #training_pickle.close()
+
+        #combo = list(zip(images, labels))
+        #random.shuffle(combo)
+        #images, labels = zip(*combo)
+
+        #return images, labels
 
 def get_test_data(batch_size):
     data = []
@@ -79,23 +74,25 @@ def get_test_data(batch_size):
 
     return test_images[:batch_size * (iteration - 1)], test_labels[:batch_size * (iteration - 1)]
 
-def get_validation_data(batch_size):
-    validation_pickle = open(os.path.join(TRAINING_DIR, "validation.pkl"), 'rb')
-    images, labels = pickle.load(validation_pickle)
-    #print("Loaded validation data from pickle")
-    validation_pickle.close()
+def get_validation_data(batch_size): 
+    training_file = h5.File(TRAINING_DIR+'fruit_data.hdf5', 'r')
+    images, labels = training_file["Validation"]["images"], training_file["Validation"]["labels"]
     iteration = 1
     while batch_size * iteration < len(images):
         iteration += 1
 
     return images[:batch_size * (iteration - 1)], labels[:batch_size * (iteration - 1)]
 
-def format_data(data):
+def format_data(data, outputFile, name):
+    shape = (len(data), 100, 100, 3)
+    group = outputFile.create_group(name)
+
+    imageSet = group.create_dataset("images", shape, dtype=np.float64)
+    labelSet = group.create_dataset("labels", (len(data),), dtype="i")
+
     random.shuffle(data)
-    #Gets one batch of training data
-    image_data = np.zeros((len(data), 100, 100, 3), dtype=np.float64)
-    image_labels = np.zeros((len(data)))
+
     for index, image_label_pair in enumerate(data):
-        image_data[index] = image_label_pair[0]
-        image_labels[index] = image_label_pair[1]
-    return image_data, image_labels
+        imageSet[index,:,:,:] = image_to_array(image_label_pair[0])
+        labelSet[index] = image_label_pair[1]
+    return imageSet, labelSet
